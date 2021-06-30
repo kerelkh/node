f5b0ecbed3142e1db28ccf4d2e21e47782a49a74
kerelkh <kerelkh@gmail.com>
2021-06-30 18:34:02 +0700

    Initiate
    Initiate Project with server and routes

---
+++ app.js
@@ -0,0 +1,33 @@
+const prod = false;
+
+if(!prod){
+  require('dotenv').config();
+}
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//express config
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+//routes
+const indexRoute = require('./routes/index');
+
+
+//use routes
+app.use('/', indexRoute);
+
+
+//error 404
+app.use((req, res) => {
+  res.render('error/error404');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server Listening to port ${PORT}`);
+});
