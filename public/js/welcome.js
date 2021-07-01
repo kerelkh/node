548b02a86ee75f5c1ae5d319fde80935a784894d
kerelkh <kerelkh@gmail.com>
2021-07-01 18:14:42 +0700

    make it better and add jumbotron section

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,6 +1,6 @@
 (function(){
   const btnToggle = document.querySelector('#btnToggle');
-  const rightNav = document.querySelector('.right-nav ul');
+  const rightNav = document.querySelector('.right-nav');
 
   btnToggle.addEventListener('click', ()=>{
     btnToggle.classList.toggle('active');
