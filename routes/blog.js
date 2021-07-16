4d12a6eccd72d64e2fb44e21f8ab9f3042a32108
kerelkh <kerelkh@gmail.com>
2021-07-16 15:46:13 +0700

    newest data blogs

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { updateCount } = require('../config/util');
+const Blog = require('../config/blog');
 
-router.get('/', updateCount('Blog'), (req, res) => {
-  res.render('blog', { login: false});
+router.get('/', updateCount('Blog'), async (req, res) => {
+
+  //get data newest post
+  const newestData = await Blog.find({ status: 'publish'}, null, { skip: 0, limit: 12, sort: {date: 1}}, (err, result) => {
+    return result;
+  })
+
+  res.render('blog', { login: false, newestData: newestData});
 })
 
 module.exports = router;
